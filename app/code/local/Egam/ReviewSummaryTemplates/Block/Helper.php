5e6257ce82a89008c74e0296c5429dbf9df6b5a4
Hans Kuijpers <hans2103@gmail.com>
2013-05-26 16:40:52 +0200

    initial upload of extension

---
+++ app/code/local/Egam/ReviewSummaryTemplates/Block/Helper.php
@@ -0,0 +1,10 @@
+<?php
+
+class Egam_ReviewSummaryTemplates_Block_Helper extends Mage_Review_Block_Helper
+{
+	protected $_availableTemplates = array(
+        'default'   => 'review/helper/summary.phtml',
+        'short'     => 'review/helper/summary_short.phtml',
+		'aggregate' => 'review/helper/summary_aggregate.phtml',
+    );
+}
